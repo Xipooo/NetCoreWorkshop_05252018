f63dcf403d230ac11535bba2fdea916721465595
Steve Bishop <xipooo@gmail.com>
2018-05-25 13:56:22 -0700

    Solution implemented

--- Person.cs
+++ Person.cs
@@ -2,12 +2,39 @@ namespace NetCoreWorkshop_05252018
 {
     public class Person
     {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public string getFullName(){
-            return $"{FirstName} {MiddleName} {LastName}";
+        private int? _id;
+        public int Id
+        {
+            get
+            {
+                return _id ?? 0;
+            }
+            set
+            {
+                _id = value;
+            }
+        }
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName ?? "Unknown"; }
+            set { _firstName = value; }
+        }
+        private string _middleName;
+        public string MiddleName
+        {
+            get { return _middleName ?? "Unknown"; }
+            set { _middleName = value; }
+        }
+
+        private string _lastName;
+        public string LastName { 
+            get { return _lastName ?? "Unknown"; } 
+            set { _lastName = value; }
+            }
+        public string getInfo()
+        {
+            return $"Id: {Id}  First Name: {FirstName}  Middle Name: {MiddleName}  Last Name: {LastName}";
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,11 +6,16 @@ namespace NetCoreWorkshop_05252018
     {
         static void Main(string[] args)
         {
-            Person steve = new Person { Id = 1, FirstName = "Steve", LastName="Bishop"};
-            Console.WriteLine(steve.getFullName());
+            Person steve = new Person { FirstName = "Steve", LastName = "Bishop" };
+            Console.WriteLine(steve.getInfo());
 
             Person amanda = null;
-            Console.WriteLine(amanda.getFullName());
+            Console.WriteLine(amanda?.getInfo() ?? "Amanda Doesn't Exist");
+
+            // if (amanda != null)
+            // {
+            //     Console.WriteLine(amanda.getInfo());
+            // }
         }
     }
 }
